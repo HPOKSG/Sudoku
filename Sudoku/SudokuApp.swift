468e3822e375ab3d8ac41d2aead7af1f416b82a3
Huu Phuoc <s3878270@rmit.edu.vn>
2023-08-22 21:48:18 +0700

    Initial Commit

---
+++ Sudoku/SudokuApp.swift
@@ -0,0 +1,17 @@
+//
+//  SudokuApp.swift
+//  Sudoku
+//
+//  Created by Hữu Phước  on 22/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct SudokuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
